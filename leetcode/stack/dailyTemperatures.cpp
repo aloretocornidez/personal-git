b8267d24944dd916da71e6f4947dfc56054d1a36
Alan Loreto Cornidez <aloretocornidez@arizona.edu>
2023-10-04 16:40:52 -0700

    leetcode progress

---
+++ leetcode/stack/dailyTemperatures.cpp
@@ -0,0 +1,50 @@
+/*
+    Given array of temps, return an array w/ # of days until warmer
+    Ex. temperature = [73,74,75,71,69,72,76,73] -> [1,1,4,2,1,1,0,0]
+
+    Monotonic decr stack, at each day, compare incr from prev days
+
+    Time: O(n)
+    Space: O(n)
+*/
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+
+        int n = temperatures.size();
+
+        // pair[index, temperature]
+        stack<pair<int, int>> stk;
+        vector <int> result(n);
+
+        for(int i = 0; i < n; i++)
+        {
+            int currentDay = i;
+            int currentTemp = temperatures[i];
+
+            // Iterate through the stack until the temperature is 
+            while(!stk.empty() && stk.top().second < currentTemp)
+            {
+                int previousTemp = stk.top().second;
+                int previousDay = stk.top().first;
+
+                stk.pop();
+
+                result[previousDay] = currentDay - previousDay;
+                
+            }
+
+            stk.push({currentDay, currentTemp});
+
+        }
+        
+        return result;   
+    }
+};
